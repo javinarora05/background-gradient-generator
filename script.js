650d9fab30c2409fe7db7ef071d7d032e7faaed8
Javin Arora <javinarora05@gmail.com>
2025-12-05 01:19:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+let rgb1 = "#000"
+let rbg2 = "#fff"
+let copyDiv = document.querySelector(".code-box");
+
+
+
+let hexValues = () => {
+    let values = "0123456789abcdef";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += values[Math.floor(Math.random() * 16)];
+    }
+    return color;
+};
+
+let changeColor1 = () => {
+     rgb1 = hexValues();
+    document.body.style.backgroundImage= `linear-gradient(to right, ${rgb1}, ${rgb2})`;
+    copyDiv.innerHTML = ` background-image: linear-gradient(to right, ${rgb1}, ${rgb2})`;
+};
+
+let changeColor2 = () => {
+    rgb2 = hexValues();
+     document.body.style.backgroundImage= `linear-gradient(to right, ${rgb1}, ${rgb2})`;
+     copyDiv.innerHTML = ` background-image: linear-gradient(to right, ${rgb1}, ${rgb2})`;
+};
+
+btn1.addEventListener("click", changeColor1);
+btn2.addEventListener("click", changeColor2);
